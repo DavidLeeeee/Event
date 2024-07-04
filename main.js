1df6f625886fb444e33f58e2cf2f8eedb681c301
Seokhee <46704693+DavidLeeeee@users.noreply.github.com>
2024-07-04 17:57:44 +0900

    Update main.js

--- main.js
+++ main.js
@@ -1,52 +1,60 @@
-document.addEventListener("DOMContentLoaded", () => {
-  const boxOptions = {
-    outerBox: {
-      capture: false,
-      useStopPropagation: false,
-      useStopImmediatePropagation: false,
-      usePreventDefault: false,
-    },
-    middleBox: {
-      capture: false,
-      useStopPropagation: false,
-      useStopImmediatePropagation: false,
-      usePreventDefault: false,
-    },
-    innerBox1: {
-      capture: false,
-      useStopPropagation: false,
-      useStopImmediatePropagation: false,
-      usePreventDefault: false,
-    },
-    innerBox2: {
-      capture: false,
-      useStopPropagation: false,
-      useStopImmediatePropagation: false,
-      usePreventDefault: false,
-    },
-  };
+const boxOptions = {
+  outerBox: {
+    useStopPropagation: false,
+    useStopImmediatePropagation: false,
+    usePreventDefault: false,
+  },
+  middleBox: {
+    useStopPropagation: false,
+    useStopImmediatePropagation: false,
+    usePreventDefault: false,
+  },
+  innerBox1: {
+    useStopPropagation: false,
+    useStopImmediatePropagation: false,
+    usePreventDefault: false,
+  },
+  innerBox2: {
+    useStopPropagation: false,
+    useStopImmediatePropagation: false,
+    usePreventDefault: false,
+  },
+};
+let captureMode = false;
 
-  // 이벤트 리스너를 추가하는 함수
-  function addEventListenerToSelector(selector, eventType, handler) {
+document.addEventListener("DOMContentLoaded", () => {
+  addEventToSelector(
+    "click", //Event Type
+    ".outerBox, .middleBox, .innerBox1, .innerBox2", //DOM
+    handleClick //function
+  );
+
+  /* 이벤트 등록 / 제거 함수 */
+  function addEventToSelector(eventType, selector, handler) {
     const elements = document.querySelectorAll(selector);
     elements.forEach((element) => {
-      element.addEventListener(eventType, handler, boxOptions[element.className].capture);  // Use the capture option
+      element.addEventListener(eventType, handler, captureMode); // Use the capture option
+    });
+  }
+  function removeEventFromSelector(eventType, selector, handler) {
+    const elements = document.querySelectorAll(selector);
+    elements.forEach((element) => {
+      element.removeEventListener(eventType, handler, captureMode); // Remove the event listener
     });
   }
 
-  // Click Event Handler
   function handleClick(event) {
-    const className = event.currentTarget.className;
+    const className = event.currentTarget.className; //발생 요소
     const options = boxOptions[className];
-    const optionSetterName = event.target.className;
+    const optionSetterName = event.target.className; //클릭한 요소
 
     if (options.usePreventDefault) {
       event.preventDefault();
     }
-
     if (options.useStopImmediatePropagation) {
       event.stopImmediatePropagation();
-    } else if (options.useStopPropagation) {
+    }
+    if (options.useStopPropagation) {
       event.stopPropagation();
     }
 
@@ -63,79 +71,63 @@ document.addEventListener("DOMContentLoaded", () => {
     document.querySelector(".console-body").appendChild(logEntry);
   }
 
-  // Add Event Listener
-  addEventListenerToSelector(
-    ".outerBox, .middleBox, .innerBox1, .innerBox2",
-    "click",
-    handleClick
-  );
-
   // Console Empty
   const clearButton = document.getElementById("clear-button");
   clearButton.addEventListener("click", () => {
     document.querySelector(".console-body").textContent = "";
   });
 
-
-  // Remove event listeners function
-  function removeEventListenerFromSelector(selector, eventType, handler) {
-    const elements = document.querySelectorAll(selector);
-    elements.forEach((element) => {
-      element.removeEventListener(eventType, handler, boxOptions[element.className].capture);
-    });
-  }
-  // Reattach event listeners
-  function reattachEventListeners() {
-    removeEventListenerFromSelector(
-      ".outerBox, .middleBox, .innerBox1, .innerBox2",
-      "click",
-      handleClick
-    );
-    addEventListenerToSelector(
-      ".outerBox, .middleBox, .innerBox1, .innerBox2",
-      "click",
-      handleClick
-    );
-  }
-
-  
   // Option Button Event Handler
   document
     .querySelectorAll(".outerBox, .middleBox, .innerBox1, .innerBox2")
     .forEach((element) => {
       element.addEventListener("click", (event) => {
-        const selectedBox = event.target.className;
-        const optionButtons = document.querySelectorAll(".option button");
+        const selectedBox = event.target.className; //선택된 박스
+        const optionButtons = document.querySelectorAll(".option button"); //선택한 옵션
         optionButtons.forEach((button) => {
           button.onclick = () => {
-            const optionName = button.textContent.trim();
-            let optionValue;
+            const optionName = button.getAttribute("data-option");
+            let optionValue; //값 안내하기
             switch (optionName) {
-              case "stopPropagation()":
+              case "stopPropagation":
                 boxOptions[selectedBox].useStopPropagation =
                   !boxOptions[selectedBox].useStopPropagation;
                 optionValue = boxOptions[selectedBox].useStopPropagation;
                 break;
-              case "stopImmediatePropagation()":
+              case "stopImmediatePropagation":
                 boxOptions[selectedBox].useStopImmediatePropagation =
                   !boxOptions[selectedBox].useStopImmediatePropagation;
-                optionValue = boxOptions[selectedBox].useStopImmediatePropagation;
+                optionValue =
+                  boxOptions[selectedBox].useStopImmediatePropagation;
                 break;
-              case "preventDefault()":
+              case "preventDefault":
                 boxOptions[selectedBox].usePreventDefault =
                   !boxOptions[selectedBox].usePreventDefault;
                 optionValue = boxOptions[selectedBox].usePreventDefault;
                 break;
-              case "Toggle(Bubble/Capture)":
-                boxOptions[selectedBox].capture =
-                  !boxOptions[selectedBox].capture;
-                optionValue = boxOptions[selectedBox].capture;
-                reattachEventListeners();  
+              case "toggleEventPhase":
+                removeEventFromSelector(
+                  "click",
+                  ".outerBox, .middleBox, .innerBox1, .innerBox2",
+                  handleClick
+                );
+                /*
+                 * 전파 모드 변경 시에는 이벤트를 제거한 뒤 부착해야한다. (새로 이벤트 등록이 필요)
+                 * 전파 모드가 다르다면, 이벤트 제거가 불가능하다. 즉, captureMode가 같아야 리스너 제거 가능
+                 */
+                captureMode = !captureMode;
+                addEventToSelector(
+                  "click",
+                  ".outerBox, .middleBox, .innerBox1, .innerBox2",
+                  handleClick
+                );
+                optionValue = captureMode ? "Capture" : "Bubble";
                 break;
             }
             updateConsole(
               `${selectedBox} & ${optionName} 설정: ${optionValue}`
             );
+            console.log(boxOptions);
           };
         });
       });
